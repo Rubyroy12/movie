161cd7795ed5c3927026ccb42e5fcd459e5ee711
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-11 23:54:26 +0300

    config user details

--- config.py
+++ config.py
@@ -14,11 +14,12 @@ class Config:
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
-  
+
+
 class ProdConfig(Config):
-        SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-        
-        
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL","")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI =SQLALCHEMY_DATABASE_URI.replace("postgres://","postgresql://",1)
 
 
 class TestConfig(Config):
