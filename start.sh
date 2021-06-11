bed19f4da9991d0d630ee546b2d3e8f6b8bfbdda
Rubyroy12 <ibrakipz7@gmail.com>
2021-06-12 00:18:15 +0300

    change login email

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MOVIE_API_KEY=5b554e67624057bcd681bdca1bb261ec
 export SECRET_KEY=Royal1224
-export MAIL_USERNAME='ibrakipz7@gmail.com'
-export MAIL_PASSWORD='Dormans2'
+export MAIL_USERNAME='ibrahimkiprotich81@gmail.com'
+export MAIL_PASSWORD='ibrahim81'
 
 python3.8 manage.py server
